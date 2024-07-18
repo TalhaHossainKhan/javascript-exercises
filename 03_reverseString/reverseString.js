90c76cd66ec0afb7ba68a0124e1a6f67aa56748b
TalhaHossainKhan <talhahossain75@gmail.com>
2024-07-18 00:07:04 -0400

    did exercises 01- 07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const arrayString = []
+    for (i = 0; i < string.length; i++){
+        arrayString[i] = string[i]
+    }
+
+    let output = ""
+    for (i = arrayString.length - 1; i > -1; i--){
+        output = `${output}${arrayString[i]}`
+    }
+
+    return output
 
 };
 
